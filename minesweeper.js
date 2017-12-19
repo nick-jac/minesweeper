a18dfaa0ef094d82fc5fe79ca191e58c05179be8
Nick Jacobson <33440907+nick-jac@users.noreply.github.com>
2017-12-19 20:22:13 +1300

    running minesweeper

--- minesweeper.js
+++ minesweeper.js
@@ -1,20 +1,21 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-var board = {} 
- board['cells'] = [
-  {hidden: true, isMine: false, row:0, col:0}, {hidden: true, isMine: false, row:0, col:1}, {hidden: true, isMine: true, row:0, col:2}, {hidden: true, isMine: true, row:0, col:3},
-  {hidden: true, isMine: true, row:1, col:0}, {hidden: true, isMine: true, row:1, col:1}, {hidden: true, isMine: false, row:1, col:2}, {hidden: true, isMine: true, row:1, col:3},
-  {hidden: true, isMine: false, row:2, col:0}, {hidden: true, isMine: false, row:2, col:1}, {hidden: true, isMine: true, row:2, col:2}, {hidden: true, isMine: true, row:2, col:3},
-  {hidden: true, isMine: true, row:3, col:0}, {hidden: true, isMine: true, row:3, col:1}, {hidden: true, isMine: false, row:3, col:2}, {hidden: true, isMine: true, row:3, col:3}
+ var board = {  } 
+ board.cells = [
+   {hidden:true, row: 0, col: 0, isMine: true}, {hidden:true, row: 0, col:1, isMine: true}, {hidden:true, row: 0, col:2, isMine: false},  {hidden:true, row: 0, col:3, isMine: true},
+   {hidden:true, row: 1, col: 0, isMine: false}, {hidden:true, row: 1, col:1, isMine: false}, {hidden:true, row: 1, col:2, isMine: false}, {hidden:true, row: 1, col:3, isMine: false},
+   {hidden:true, row: 2, col: 0, isMine: true}, {hidden:true, row: 2, col:1, isMine: true}, {hidden:true, row: 2, col:2, isMine: true},  {hidden:true, row: 2, col:3, isMine: false},
+   {hidden:true, row: 3, col: 0, isMine: false}, {hidden:true, row: 3, col:1, isMine: false}, {hidden:true, row: 3, col:2, isMine: false}, {hidden:true, row: 3, col:3, isMine: false}
   ];
-
 function startGame () {
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
- for ( var i = 0; i < board.cells.length; i++) {
-board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
+  for (var i = 0; i < board.cells.length; i++) {
+    board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
   }
+  document.addEventListener('click', checkForWin)
+  document.addEventListener('rightclick', checkForWin)
 }
 
 // Define this function to look for a win condition:
@@ -22,29 +23,53 @@ board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
 function checkForWin () {
+  
+  var hasMines = 0;
+  for (var i = 0; i < board.cells.length; i++) {
+     var isMine =board.cells[i].isMine;
+     var isMarked =board.cells[i].isMarked;
+    
+  if ((isMine === true) && (isMarked === true)){
+    hasMines++
+    }
+  }
+  var notMined= 0;
 
+  for (var i = 0; i <board.cells.length; i++) {
+  var isMine = board.cells[i].isMine;
+  var isHidden = board.cells[i].hidden;
+  
+  if ((isMine === false) && (isHidden == false)) {
+    notMined++;
+    }
+    if ((hasMines === 6) || (notMined === 10)) {
+      lib.displayMessage('You win!');
+    }
+  }
+}
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
   //   lib.displayMessage('You win!')
-}
+
 
 // Define this function to count the number of mines around the cell
 // (there could be as many as 8). You don't have to get the surrounding
 // cells yourself! Just use `lib.getSurroundingCells`: 
 //
-// var surrounding = lib.getSurroundingCells(cell.row, cell.col)
+//   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
 //
 // It will return cell objects in an array. You should loop through 
 // them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines (cell) {
   var surroundingCells = lib.getSurroundingCells(cell.row, cell.col);
-  var count = 0;
+  
+    var count = 0;
 
-  for (var i=0; i < surroundingCells.length; i++){
-    if (surroundingCells[i].isMine){
-      count++;
+    for (var i=0; i <surroundingCells.length; i++) {
+      if (surroundingCells[i].isMine){
+         count++;
     }  
-  }
-  return count;
+   }
+   return count;
 }
 
